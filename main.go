8be6200952fd6fa1b0d0f2492e188cd89d4d9322
markcampv <mnmvincent@gmail.com>
2024-02-21 13:56:56 -0500

    simplify main.go

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"debug-redactor/cmd"
-	"fmt"
-	"os"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1) // Exits the program with a status code of 1 to indicate an error occurred.
+	cmd.Execute()
 	}
 }
